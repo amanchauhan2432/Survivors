890586bc317d357f525375db567035a038b0d413
amanchauhan2432 <amanchauhan2432@gmail.com>
2022-09-07 15:17:52 +0530

    Final Push

---
+++ Source/Survivors/SurvivorsGameModeBase.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SurvivorsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURVIVORS_API ASurvivorsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void PawnKilled(APawn* PawnKilled);
+};
